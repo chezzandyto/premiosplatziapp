c30e8dc316d7f64adb3b6b695f763008e574cfcd
chezzandyto <andres_toapanta@hotmail.com>
2022-02-15 21:50:00 -0500

    creado los tests de polls para pasado y ahora

--- premiosplatziapp/polls/tests.py
+++ premiosplatziapp/polls/tests.py
@@ -11,3 +11,15 @@ class QuestionModelTests(TestCase): #heredar de testcase nos permiten traer una 
         time = timezone.now() + datetime.timedelta(days=30)
         future_question = Question(question_text="Quien es el mejor Course Director de Platzi",pub_date=time)
         self.assertIs(future_question.was_published_recently(),False) # verifica con el metodo assert si el resultado de la funcion es igual a false
+
+    def test_was_pulished_recently_with_today_questions(self):
+        """was_published_recently should return True for questions whose pub_date is today"""
+        time = timezone.now()
+        question = Question(question_text="Quien es el mejor Course Director de Platzi",pub_date=time)
+        self.assertEquals(question.was_published_recently(),True)
+
+    def test_was_published_recently_with_past_questions(self):
+        """was_published_recently should return flase for questions whose pub_date was yesterday or more"""
+        time = timezone.now() - datetime.timedelta(days=10)
+        past_question = Question(question_text="Quien es el mejor Course Director de Platzi",pub_date=time)
+        self.assertEquals(past_question.was_published_recently(),False)
