aebbf3c34a0531467d847775626e5daf6b66a519
chezzandyto <andres_toapanta@hotmail.com>
2022-08-30 22:09:15 -0500

    last commit

--- premiosplatziapp/polls/admin.py
+++ premiosplatziapp/polls/admin.py
@@ -1,5 +1,18 @@
+from dataclasses import fields
 from django.contrib import admin
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question) #esto hace que el modelo sea registrado en el administrador de datos
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+#personaliza como se ve el modelo Question en el administrador de django
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["pub_date", "question_text"]
+    inlines = [ChoiceInline] #esta linea sirve para en el admin de django ubicar tambien las choices y crearlas de una vez
+
+
+
+
+admin.site.register(Question, QuestionAdmin) #esto hace que el modelo sea registrado en el administrador de datos
 
